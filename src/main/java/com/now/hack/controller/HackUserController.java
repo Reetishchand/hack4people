d04a827899cba940deec184e0db6783b91a5e98c
Reetish Chand <reetishchnad.gp@gmail.com>
2020-08-21 13:42:21 -0500

    added repo and servicde classes

--- src/main/java/com/now/hack/controller/HackUserController.java
+++ src/main/java/com/now/hack/controller/HackUserController.java
@@ -2,16 +2,20 @@ package com.now.hack.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.now.hack.Hack4PeopleApplication;
+import com.now.hack.service.HackUserService;
 
 @RestController
 @RequestMapping(path = "/user")
 public class HackUserController {
     private static final Logger logger = LogManager.getLogger(Hack4PeopleApplication.class);
+@Autowired
+private HackUserService  hackuserService;
 
 	@GetMapping("/test")
     public String index() {
---
+++ src/main/java/com/now/hack/service/HackUserService.java
@@ -0,0 +1,17 @@
+package com.now.hack.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.now.hack.Hack4PeopleApplication;
+import com.now.hack.repository.HackUserRepository;
+
+@Service
+public class HackUserService {
+	private static final Logger logger = LogManager.getLogger(Hack4PeopleApplication.class);
+	@Autowired
+	private   HackUserRepository hackUserRepository;
+
+}
